fa140096854192dca0a37f4fc4b44fec4f70b353
AkhunovAziz <akhunovaziz735@gmail.com>
2023-10-21 21:58:06 +0500

    Добавьте файлы проекта.

---
+++ LINQ/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ.Models
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? LastName { get; set; }
+        public int Age { get; set; }
+        public float Salary { get; set; }
+        public string Possition { get; set; }
+        public List<string> ProgrammingLanguages { get; set; }
+        public static List<Employee> GetEmployee()
+        {
+            var employees = new List<Employee>()
+            {
+                new Employee {Id = 1,Name = "Aziz",LastName = "Akhunov",Age = 17,Salary = 50000,Possition = "TeamLead",ProgrammingLanguages  = new List<string>{ "C#","Python"} },
+                new Employee {Id = 2,Name = "Akbar",LastName = "Oxunov",Age = 24,Salary = 100,Possition = "Junior",ProgrammingLanguages  = new List<string>{ "Java"}},
+                new Employee {Id = 3,Name = "Nurmuxammad",LastName = "Sharobbiddinov",Age = 21,Salary = 500,Possition = "Middle",ProgrammingLanguages  = new List<string>{"JavaScript"}},
+                new Employee {Id = 4,Name = "Azizbek",LastName = "Rahmatullayev",Age = 23,Salary = 150,Possition = "StrongJunior",ProgrammingLanguages  = new List<string>{ "C++"}}
+            };
+            return employees;
+        }
+    }
+}
---
+++ LINQ/Quaries.cs
@@ -0,0 +1,53 @@
+﻿using LINQ.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    internal class Quaries
+    {
+        public static void Run()
+        {
+            /* Syntaxes
+            List<int> numbers = new List<int>() { 2, 3, 4, 5, 6, 7, 8, 1234, 4458 };
+
+            ////Method Syntax
+
+            //IEnumerable<int> query = numbers.Where(x => x % 2 == 0) .ToList();
+
+          
+            //Query syntax
+
+            //var query = from number in numbers
+            //            where number % 2 == 0    //select * from numbers where x % 2 == 0;
+            //            select number;
+            
+            //Mixed syntax
+
+            var query = (from  number in numbers select number).Where(number => number % 2 == 0);
+
+
+            foreach (int number in query)
+            {
+                Console.WriteLine(number);
+            }
+            */
+
+            //var employeeSalary = (from employee in Employee.GetEmployee()
+            //                       select (employee.Name,employee.LastName,employee.Salary * 12)).ToList();
+
+            //List<float> salaries = new List<float>();
+
+            //foreach (var employee in employeeSalary)
+            //{
+            //    Console.WriteLine(employee);
+            //    salaries.Add(employee.Item3);
+            //}
+            //Console.WriteLine(salaries.Max());
+        }
+    }
+}
