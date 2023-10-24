d41c214ac3086e08cac45f55802b14c9fa641ad9
AkhunovAziz <akhunovaziz735@gmail.com>
2023-10-24 21:02:31 +0500

    Last Day

---
+++ LINQ/Fourth_day/Quaries_4.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ.Fourth_day
+{
+    public class Quaries_4
+    {
+        public static void Run()
+        {
+            var collection = Enumerable.Range(5, 5);//if we are using repeat Answer: 5 5 5 5 5 
+            Console.WriteLine("Total count: {0}",collection.Count());
+
+            for (int i = 0; i < collection.Count(); i++)
+                Console.WriteLine("Value At Index: {0} : {1}",i,collection.ElementAt(i));
+        }
+    }
+}
--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,7 +1,9 @@
 ﻿using LINQ;
+using LINQ.Fourth_day;
 using LINQ.second_day;
 using LINQ.Third_day;
 
 //Quaries.Run();
 //Quaries_2.Run();
-Quaries_3.Run();
+//Quaries_3.Run();
+Quaries_4.Run();
